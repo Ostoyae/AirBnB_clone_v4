29f68b386fd1fd0d42e24399ccd68b11f6ddfec6
Fernando Gonzalez-Morales <542@holbertonschool.com>
2019-05-20 17:15:43 +0000

    Semistandard camel case fixes

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,117 +1,110 @@
 // Get Users for since place only has the user_id value
 let users = {};
 $.get('http://0.0.0.0:5001/api/v1/users/')
-    .done((data) => {
-        data.map((usr) => {
-            users[usr.id] = usr;
-        });
-        console.log('Received user data and mapped to dictionary');
+  .done((data) => {
+    data.map((usr) => {
+      users[usr.id] = usr;
     });
+    console.log('Received user data and mapped to dictionary');
+  });
 
 $(document).ready(function () {
-    // api call to get places
-    get_places_search();
+  // api call to get places
+  get_places_search();
 
-    $.get('http://0.0.0.0:5001/api/v1/status/', (resp) => {
-        if (resp.status === 'OK') {
-            $('DIV#api_status').addClass('available');
-        } else {
-            $('DIV#api_status').removeClass('available');
-        }
-    });
-
-
-    let amenities = {};
-    $('DIV.amenities UL.popover li').each(function () {
-        if ($(this).find(':checkbox').prop('checked')) {
-            amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-        }
-    });
-
-    if ($.isEmptyObject(amenities)) {
-        $('Div.amenities h4').html('&nbsp;');
+  $.get('http://0.0.0.0:5001/api/v1/status/', (resp) => {
+    if (resp.status === 'OK') {
+      $('DIV#api_status').addClass('available');
     } else {
-        $('Div.amenities h4').text(Object.values(amenities));
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  let amenities = {};
+  $('DIV.amenities UL.popover li').each(function () {
+    if ($(this).find(':checkbox').prop('checked')) {
+      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+    }
+  });
+
+  if ($.isEmptyObject(amenities)) {
+    $('Div.amenities h4').html('&nbsp;');
+  } else {
+    $('Div.amenities h4').text(Object.values(amenities));
+  }
+
+  $('DIV.amenities UL.popover li').click(function () {
+    let cd = $(this).find(':checkbox');
+    let name = $(this).attr('data-name');
+    let id = $(this).attr('data-id');
+    if (!cd.prop('checked')) {
+      delete amenities[id];
+    } else {
+      amenities[id] = name;
+      // console.log($(this).attr('data-name'));
     }
 
-    $('DIV.amenities UL.popover li').click(function () {
-        let cd = $(this).find(':checkbox');
-        let name = $(this).attr('data-name');
-        let id = $(this).attr('data-id');
-        if (!cd.prop('checked')) {
-            delete amenities[id];
-        } else {
-            amenities[id] = name;
-            // console.log($(this).attr('data-name'));
-        }
-
-        console.log(amenities);
-        if ($.isEmptyObject(amenities)) {
-            $('Div.amenities h4').html('&nbsp;');
-        } else {
-            $('Div.amenities h4').text(Object.values(amenities));
-        }
-    });
+    console.log(amenities);
+    if ($.isEmptyObject(amenities)) {
+      $('Div.amenities h4').html('&nbsp;');
+    } else {
+      $('Div.amenities h4').text(Object.values(amenities));
+    }
+  });
 });
 
-function get_places_search(json={}) {
-    //remove existing articles, however a naive solution.
-    $('section.places article').remove();
-    $.ajax({
-            type: "POST",
-            url: 'http://0.0.0.0:5001/api/v1/places_search/',
-            data: JSON.stringify(json),
-            contentType: 'application/json',
-            success: function (resp) {
-                create_article(resp);
-            }
-        }
-    )
-        .done(() => {
-            console.log('Received places data');
-        })
-        .fail((resp) => {
-            console.log(resp.responseText);
-        });
-
+function fetchPlaces (json = {}) {
+  // remove existing articles, however a naive solution.
+  $('section.places article').remove();
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    data: JSON.stringify(json),
+    contentType: 'application/json',
+    success: function (resp) {
+      createArticle(resp);
+    }
+  }
+  )
+    .done(() => {
+      console.log('Received places data');
+    })
+    .fail((resp) => {
+      console.log(resp.responseText);
+    });
 }
 
-
-// create_article - generates a html article for each place
+// createArticle - generates a html article for each place
 //
 // data - List of places from api
-function create_article(data) {
+function createArticle (data) {
+  let dest = $('section.places');
 
-    let dest = $('section.places');
-
-    data.forEach((place) => {
-        dest.append(set_html(place, users[place.user_id]));
-    });
-
-
+  data.forEach((place) => {
+    dest.append(setHTML(place, users[place.user_id]));
+  });
 }
 
-// set_html - Take a html template and set the values accordingly
+// setHTML - Take a html template and set the values accordingly
 //
 // html - Place object
 // user - User Object associated with the place.
 //
 // Returns Article.
-function set_html(place, user) {
-    let html = $($.parseHTML(ArticleHTML));
-    let article = html.clone();
-    let usr_name = users ? `${user.first_name} ${user.last_name}` : 'john doe';
+function setHTML (place, user) {
+  let html = $($.parseHTML(ArticleHTML));
+  let article = html.clone();
+  let username = users ? `${user.first_name} ${user.last_name}` : 'john doe';
 
-    article.find('.title h2').text(place.name).css('overflow-wrap', 'anywhere');
-    article.find('.price_by_night').text(place.price_by_night);
-    article.find('.max_guest').find('br').after(`${place.max_guest} Guests`);
-    article.find('.number_rooms').find('br').after(`${place.number_rooms} Bedrooms`);
-    article.find('.number_bathrooms').find('br').after(`${place.number_bathrooms} Bathroom`);
-    article.find('.user strong').text(`Owner: ${usr_name}`);
-    article.find('.description').html(place.description);
+  article.find('.title h2').text(place.name).css('overflow-wrap', 'anywhere');
+  article.find('.price_by_night').text(place.price_by_night);
+  article.find('.max_guest').find('br').after(`${place.max_guest} Guests`);
+  article.find('.number_rooms').find('br').after(`${place.number_rooms} Bedrooms`);
+  article.find('.number_bathrooms').find('br').after(`${place.number_bathrooms} Bathroom`);
+  article.find('.user strong').text(`Owner: ${username}`);
+  article.find('.description').html(place.description);
 
-    return article;
-
+  return article;
 }
 
 const ArticleHTML = '<article>\n' +
